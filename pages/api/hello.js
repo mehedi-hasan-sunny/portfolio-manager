c62b21a9461fb5b8b0e9b1eaaf84546f123862c6
Mehedi <hasansunny.mehedi@gmail.com>
2021-12-25 23:41:04 +0600

    firebase admin solve 1.

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,4 +1,16 @@
-import {firebaseAdmin} from "!/firebaseDb/firebaseAdmin"
+import * as firebaseAdmin from 'firebase-admin';
+
+const serviceAccount = require('../../firebaseDb/shohanur-protfolio-firebase-adminsdk-ppaf1-72e6aba1a3.json');
+
+if (!firebaseAdmin.apps.length) {
+	firebaseAdmin.initializeApp({
+		credential: firebaseAdmin.credential.cert({
+			privateKey: serviceAccount.private_key,
+			clientEmail: serviceAccount.client_email,
+			projectId: serviceAccount.project_id,
+		}),
+	});
+}
 
 export default async function handler(req, res) {
 	
