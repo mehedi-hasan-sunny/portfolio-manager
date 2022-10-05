bbb77c0831c00546daaeb55c197b9e99ad997fa7
Mehedi <hasansunny.mehedi@gmail.com>
2022-10-06 02:13:25 +0600

    Deep merge added for settings default data and config moved to /tmp/ folder due vercel read only error

--- pages/api/admin/settings/index.js
+++ pages/api/admin/settings/index.js
@@ -2,21 +2,34 @@ import {ValidateToken} from "../../../../helpers/api/AuthCheck";
 import MethodNotAllowedException from "../../../../helpers/CustomError";
 import {errorRes, successRes} from "../../../../helpers/jsonResponse";
 import * as path from "path";
+import deepmerge from "deepmerge";
+
 const fs = require('fs');
-const SETTINGS_PATH = path.join(process.cwd(),["config","settings.json"].join(path.sep));
+
+let SETTINGS_PATH = '';
+if (process && process.env.NODE_ENV === 'development') {
+	SETTINGS_PATH = path.join(process.cwd(), ["config", "settings.json"].join(path.sep));
+} else {
+	SETTINGS_PATH = "/tmp/settings.json";
+}
+
 let settingsData = {};
 
 try {
-	settingsData = require("../../../../config/settings.json");
+	if (process && process.env.NODE_ENV === 'development') {
+		settingsData = require("../../../../config/settings.json");
+	} else {
+		settingsData = require("/tmp/settings.json");
+	}
 } catch (e) {
 }
 const settingsDefault = require("../../../../config/settingsDefault.json");
-settingsData = {...settingsDefault, ...settingsData};
+settingsData = deepmerge(settingsDefault, settingsData);
 
 
 export default async function handler(req, res) {
 	const authCheck = await ValidateToken({req, res});
-	if(authCheck !== true){
+	if (authCheck !== true) {
 		return authCheck
 	}
 	
@@ -73,7 +86,7 @@ export default async function handler(req, res) {
 	}
 }
 
-const setNestedProp = (obj = {}, [first, ...rest] , value) => ({
+const setNestedProp = (obj = {}, [first, ...rest], value) => ({
 	...obj,
 	[first]: rest.length
 			? setNestedProp(obj[first], rest, value)
